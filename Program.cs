258fc0f3add6dbd5de86f39f2c943102e6a28050
IvaKrapiva <iva_krapiva@list.ru>
2025-04-22 00:41:02 +0500

    wrld

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
             // This is a simple C# console application.
             Console.WriteLine ("This is a test of the C# console application.");
 
--- obj/Debug/net9.0/git-course.AssemblyInfo.cs
+++ obj/Debug/net9.0/git-course.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("git-course")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4145f762973f5e7953e61be17a17756d718821ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3019a636a379b8b3a69cb6891408b533da5c6d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("git-course")]
 [assembly: System.Reflection.AssemblyTitleAttribute("git-course")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
